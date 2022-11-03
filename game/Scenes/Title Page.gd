d9400cdd324f85f557aaed7d4e96f9e963898b1b
Matthew <matthewdlr4@gmail.com>
2022-11-02 21:04:59 -0400

    Cut Scene
    Made it so we can add cut scenes. But it works.

--- game/Scenes/Title Page.gd
+++ game/Scenes/Title Page.gd
@@ -6,12 +6,9 @@ func _ready():
 
 
 func _on_Button_pressed():
-<<<<<<< Updated upstream
-	get_tree().change_scene("res://Scenes/Resteraunt.tscn")
-=======
 	var scene = load("res://Scenes/CutScene.tscn")
 	var instance = scene.instance()
 	instance.SceneSwitch = load("res://Scenes/Resteraunt.tscn")
 	instance.cutscene = 0
 	add_child(instance)
->>>>>>> Stashed changes
+
