331da441a61a0436be08ab5ff92b16bee0b6913d
WestonBond <owenmctoen@gmail.com>
2022-11-04 21:31:07 -0400

    dialogue isnt done but cody fucked it up ok

---
+++ game/Scenes/Text House.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var dialogue_1 = [
+	{
+		"Name": "John",
+		"Choices": ["",""],
+		"Text": "Hello, this is a test",
+	},
+	{
+		"Name": "Chore",
+		"Choices": ["First","Second"],
+		"Text": "Testing choices",
+	}
+]
