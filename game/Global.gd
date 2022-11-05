25eea7f59defdbfd2de3ead2ecc60e4962ac0e20
Matthew <matthewdlr4@gmail.com>
2022-11-05 10:50:45 -0400

    Add and remove item
    Made a working add and remove item.

--- game/Global.gd
+++ game/Global.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 var playerBalance = 100000000000 setget set_balance
 
 signal playerBalance_changed
@@ -9,13 +10,13 @@ func set_balance(value):
 		emit_signal("set_balance", playerBalance)
 
 
-var inventory = {}
+var inventory = {"lemon":0,"coconut":0, "strawberry":0, "cherry":0, "raspberry":0, "blackberry":0, "blueberry":0, "watermelon":0}
 
 func add_item(item,quantity):
-	inventory[item] += quantity
+	inventory[item] = inventory.get(item, 0) + int(quantity)
 
 func remove_item(item,quantity):
-	inventory[item] -= quantity
+	inventory[item] = inventory.get(item, 0) + int(quantity)
 	
 	
 	
