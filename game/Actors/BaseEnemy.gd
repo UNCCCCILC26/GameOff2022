a2232a2ded558109a4c8443714bb3fd6481213cf
FastPichu <90989686+FastPichu@users.noreply.github.com>
2022-11-07 15:05:08 -0500

    Variety of LemonHunt Fixes

--- game/Actors/BaseEnemy.gd
+++ game/Actors/BaseEnemy.gd
@@ -7,22 +7,12 @@ var screenSize = Vector2(1024,600)
 var velocity = Vector2(0,0)
 var rng = RandomNumberGenerator.new()
 
-func _ready():
-	rng.randomize()
-	var randY = rng.randf_range(50,screenSize.y-50)
-	if(rng.randi_range(0,1) == 1):
-		self.position = Vector2(-30,randY)
-		moveRight()
-	else:
-		self.position = Vector2(screenSize.x+30,randY)
-		moveLeft()
-
-func _on_LemonHitbox_area_shape_entered(a1,area,a3,a4):
+func _on_Hitbox_area_shape_entered(a1,area,a3,a4):
 	if(area.is_in_group("Scopes")):
 		scope_inside = true
 
-func _on_LemonHitbox_area_shape_exited(a1,area,a3,a4):
-	if(area.is_in_group("Scopes")):
+func _on_Hitbox_area_shape_exited(a1,area,a3,a4):
+	if(area != null and area.is_in_group("Scopes")):
 		scope_inside = false
 
 func _process(_delta):
---
+++ game/Actors/Lemon.gd
@@ -0,0 +1,11 @@
+extends "res://Actors/BaseEnemy.gd"
+
+func _ready():
+	rng.randomize()
+	var randY = rng.randf_range(50,screenSize.y-50)
+	if(rng.randi_range(0,1) == 1):
+		self.position = Vector2(-30,randY)
+		moveRight()
+	else:
+		self.position = Vector2(screenSize.x+30,randY)
+		moveLeft()
