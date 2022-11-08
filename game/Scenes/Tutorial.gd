f29669dece873e70b29d7e929fd9707b8f61e4aa
Matthew <matthewdlr4@gmail.com>
2022-11-08 18:11:37 -0500

    Tom Fuckery: The Musical

--- game/Scenes/Tutorial.gd
+++ game/Scenes/Tutorial.gd
@@ -16,6 +16,13 @@ func updateBalance():
 	$RichTextLabel.text = "Balance: " + str(Global.playerBalance)
 
 func _process(delta):
+	if !meetCustomer:
+		$"Main/Ice/DetectBox".position.y = 1000
+	if !stationOne:
+		$"Main/Drink/DetectBox".position.y = 1000
+	if !stationTwo:
+		$"Main/Flavor/DetectBox".position.y = 1000
 	if stationThree:
 		tutorial = true
+		print("Tutorial Completed")
 
