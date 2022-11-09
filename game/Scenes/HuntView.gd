3c0df6f762468f1988bae7accfa9d475c6e9e047
FastPichu <90989686+FastPichu@users.noreply.github.com>
2022-11-08 23:08:23 -0500

    Moving camera works for x dir, not y yet

--- game/Scenes/HuntView.gd
+++ game/Scenes/HuntView.gd
@@ -5,12 +5,18 @@ var mapSize = Vector2(1300,700)
 var mapOffset = Vector2((mapSize.x-screenSize.x)/2,(mapSize.y-screenSize.y)/2)
 var edgeScale = 0.8
 var cameraHomePosition = position
+var totalOffset = Vector2(mapOffset.x*2,mapOffset.y*2)
 
 func _process(delta):
 	var mousePos = get_viewport().get_mouse_position()
 	if(mousePos.x >= screenSize.x * edgeScale and mousePos.x <= screenSize.x):
 		position.x = cameraHomePosition.x + (mapOffset.x * ((mousePos.x-screenSize.x*edgeScale)/(mapOffset.x*2)))
 	elif (mousePos.x <= screenSize.x - (screenSize.x * edgeScale) and mousePos.x >= 0):
-		position.x = cameraHomePosition.x - (mapOffset.x * ((mapOffset.x*2)/(mousePos.x*edgeScale)))
+		position.x = -1*(cameraHomePosition.x - (mapOffset.x * ((mousePos.x-(mapOffset.x*2)*edgeScale)/(mapOffset.x*2))))
 	else:
 		position.x = cameraHomePosition.x
+	
+	if (mousePos.y <= screenSize.y - (screenSize.y * edgeScale) and mousePos.y >= 0):
+		position.y = -1*(cameraHomePosition.y - (mapOffset.y * (mousePos.y-totalOffset.x*edgeScale/totalOffset.x)))
+	else:
+		position.y = cameraHomePosition.y
