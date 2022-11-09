abe1ca0f67b08f765fddf8cfd10b11cdfc589f63
FastPichu <90989686+FastPichu@users.noreply.github.com>
2022-11-09 16:08:31 -0500

    Moving Camera Works (sort of)

--- game/Scenes/HuntView.gd
+++ game/Scenes/HuntView.gd
@@ -5,18 +5,31 @@ var mapSize = Vector2(1300,700)
 var mapOffset = Vector2((mapSize.x-screenSize.x)/2,(mapSize.y-screenSize.y)/2)
 var edgeScale = 0.8
 var cameraHomePosition = position
-var totalOffset = Vector2(mapOffset.x*2,mapOffset.y*2)
+
+var gap = Vector2(mapSize.x-screenSize.x,mapSize.y-screenSize.y)
+
+func _ready():
+	print(cameraHomePosition)
 
 func _process(delta):
 	var mousePos = get_viewport().get_mouse_position()
+	var distancePercentage = Vector2((gap.x - mousePos.x)/(screenSize.x-(screenSize.x*edgeScale)),(gap.y - mousePos.y)/(screenSize.y-(screenSize.y*edgeScale)))
+	if(distancePercentage.x < 0):
+		distancePercentage.x = 0
+	if(distancePercentage.y < 0):
+		distancePercentage.y = 0
+	print(mousePos)
+	
 	if(mousePos.x >= screenSize.x * edgeScale and mousePos.x <= screenSize.x):
 		position.x = cameraHomePosition.x + (mapOffset.x * ((mousePos.x-screenSize.x*edgeScale)/(mapOffset.x*2)))
-	elif (mousePos.x <= screenSize.x - (screenSize.x * edgeScale) and mousePos.x >= 0):
-		position.x = -1*(cameraHomePosition.x - (mapOffset.x * ((mousePos.x-(mapOffset.x*2)*edgeScale)/(mapOffset.x*2))))
+	elif (distancePercentage.x > 0 and distancePercentage.x < 1):
+		position.x = -1*((gap.x/2)*distancePercentage.x)
+	elif(distancePercentage.x > 1):
+		position.x = -1*(gap.x/2)
 	else:
 		position.x = cameraHomePosition.x
 	
 	if (mousePos.y <= screenSize.y - (screenSize.y * edgeScale) and mousePos.y >= 0):
-		position.y = -1*(cameraHomePosition.y - (mapOffset.y * (mousePos.y-totalOffset.x*edgeScale/totalOffset.x)))
+		position.y = -1*(gap.y*distancePercentage.y)
 	else:
 		position.y = cameraHomePosition.y
